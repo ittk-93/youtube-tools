940390c623f11780ff702e4e3026e15e1f147ba7
ittk_laptop <gaiag5245@gmail.com>
2022-06-04 09:31:14 +0900

    リスト版のループ処理を追加

--- templates.py
+++ templates.py
@@ -23,7 +23,7 @@ def template_under50(youtube, id):
 
     return
 
-def template_over50(youtube, id):
+def template_over50_a(youtube, id):
     pagetoken = None
     while True:
         # 関数ごとに自由に設定
@@ -42,4 +42,21 @@ def template_over50(youtube, id):
             pagetoken = response['nextPageToken']
         except:
             break
+    return
+
+def template_over50_b(youtube, ids):
+    #一度に読み込める回数が50件であるため、50件処理し終えるごとに次の50件を読み込む
+    ids_per50 = [ids[i:i+50] for i in range(0,len(ids),50)]
+
+    for id_x50 in ids_per50:
+        # 関数ごとに自由に設定
+        response = youtube.channels().list(
+            part = 'snippet',
+            id = ','.join(id_x50),
+            maxResults = 50,
+        ).execute()
+        time.sleep(1)
+
+        # 処理を記述
+
     return
